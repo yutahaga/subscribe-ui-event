3e358b2f1e16b533ed3a0ddfc23f7f6f531d9a49
Yuta Haga <yutahaga7@gmail.com>
2017-10-04 01:34:42 +0900

    add dist files

---
+++ dist/globalVars.js
@@ -0,0 +1,30 @@
+/**
+ * Copyright 2015, Yahoo! Inc.
+ * Copyrights licensed under the New BSD License. See the accompanying LICENSE file for terms.
+ */
+'use strict';
+
+var EventEmitter = require('eventemitter3');
+
+// Test via a getter in the options object to see if the passive property is accessed
+var supportsPassive = false;
+if (typeof window !== 'undefined' && window.addEventListener) {
+    try {
+        var opts = Object.defineProperty({}, 'passive', {
+            get: function get() {
+                supportsPassive = true;
+            }
+        });
+        window.addEventListener('test', null, opts);
+    } catch (e) {}
+}
+
+module.exports = {
+    connections: {},
+    EE: new EventEmitter(),
+    enableResizeInfo: false,
+    enableScrollInfo: false,
+    listeners: {},
+    removers: [],
+    supportsPassive: supportsPassive
+};
---
+++ dist/lib/listen.js
@@ -0,0 +1,37 @@
+/**
+ * Copyright 2015, Yahoo! Inc.
+ * Copyrights licensed under the New BSD License. See the accompanying LICENSE file for terms.
+ */
+'use strict';
+
+var globalVars = require('../globalVars');
+
+/**
+ * Cross-browser addEventListener.
+ * @method listen
+ * @param {Object} target - The target to add event listener.
+ * @param {String} eventType - The event type.
+ * @param {Function} handler - The event handler.
+ * @param {Boolean} passive - A Bool indicating whether or not to register mainEvent as passive event
+ * @return {Object} The object to be able to remove the handler.
+ */
+function listen(target, eventType, handler, passive) {
+    var add = 'addEventListener';
+    var _remove = 'removeEventListener';
+
+    if (!target.addEventListener && target.attachEvent) {
+        add = 'attachEvent';
+        _remove = 'detachEvent';
+        eventType = 'on' + eventType;
+    }
+
+    target[add](eventType, handler, passive && globalVars.supportsPassive ? { passive: true } : false);
+
+    return {
+        remove: function remove() {
+            target[_remove](eventType, handler);
+        }
+    };
+}
+
+module.exports = listen;
